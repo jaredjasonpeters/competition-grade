79d96a04b7ab4b3faac7b65c9f07f69e8995afbf
Jared Peters <jjpeters@dlfna.com>
2020-10-03 21:45:33 -0700

    refactored featured-links to populate from array of links

--- src/app/components/featured-links/featured-links.component.ts
+++ src/app/components/featured-links/featured-links.component.ts
@@ -3,13 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-featured-links',
   templateUrl: './featured-links.component.html',
-  styleUrls: ['./featured-links.component.css']
+  styleUrls: ['./featured-links.component.css'],
 })
 export class FeaturedLinksComponent implements OnInit {
+  links = [
+    {
+      title: 'speed',
+      imagePath: '../../../assets/speed_series.png',
+    },
+    {
+      title: 'power',
+      imagePath: '../../../assets/power_series.png',
+    },
+    {
+      title: 'agility',
+      imagePath: '../../../assets/agility_series.png',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

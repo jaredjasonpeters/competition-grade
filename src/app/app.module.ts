0445c4120c2f40a2d8c0239f32fa15e265638852
Jared Peters <jjpeters@dlfna.com>
2020-10-05 13:29:51 -0700

    moved distributors into own component and styles over

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,12 @@ import { BrowserModule, Title } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { HomepageComponent } from './pages/homepage/homepage.component';
 import { InstagramFeedComponent } from './components/instagram-feed/instagram-feed.component';
 import { HttpClientModule } from '@angular/common/http';
-import { FooterDetailComponent } from './footer-detail/footer-detail.component';
+import { FooterDetailComponent } from './components/footer-detail/footer-detail.component';
 import { MenuRightComponent } from './components/menu-right/menu-right.component';
 import { ImageSliderComponent } from './components/image-slider/image-slider.component';
 import { FeaturedLinksComponent } from './components/featured-links/featured-links.component';
@@ -20,6 +20,7 @@ import { FeaturedVideoComponent } from './components/featured-video/featured-vid
 import { ProductsComponent } from './pages/products/products.component';
 import { ProductComponent } from './pages/products/product/product.component';
 import { SeriesComponent } from './pages/products/series/series.component';
+import { DistributorsComponent } from './components/distributors/distributors.component';
 
 @NgModule({
   declarations: [
@@ -38,6 +39,7 @@ import { SeriesComponent } from './pages/products/series/series.component';
     ProductsComponent,
     ProductComponent,
     SeriesComponent,
+    DistributorsComponent,
   ],
   imports: [AppRoutingModule, BrowserModule, MaterialModule, HttpClientModule],
   providers: [

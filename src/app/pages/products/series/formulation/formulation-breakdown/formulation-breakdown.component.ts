65f733a3ac0481060e9746605b2d3ecde10d8f16
Jared Peters <jjpeters@dlfna.com>
2020-10-12 15:40:07 -0700

    finished animations, now work on populating pages and formulations based on url params

--- src/app/pages/products/series/formulation/formulation-breakdown/formulation-breakdown.component.ts
+++ src/app/pages/products/series/formulation/formulation-breakdown/formulation-breakdown.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 import { SeriesFormulationService } from 'src/app/shared/series-formulation.service';
 
 @Component({
@@ -7,13 +14,148 @@ import { SeriesFormulationService } from 'src/app/shared/series-formulation.serv
   styleUrls: ['./formulation-breakdown.component.css'],
 })
 export class FormulationBreakdownComponent implements OnInit {
-  formulations;
-  constructor(private seriesFormulationService: SeriesFormulationService) {}
+  formulations = {
+    primary: {
+      percentage: 0,
+      description: '',
+    },
+    secondary: {
+      percentage: 0,
+      description: '',
+    },
+    fourTurf: {
+      percentage: 0,
+      description: '',
+    },
+  };
+  @ViewChild('primaryFormulation', { static: true })
+  primaryFormulation: ElementRef;
+  @ViewChild('secondaryFormulation', { static: true })
+  secondaryFormulation: ElementRef;
+  @ViewChild('fourTurfFormulation', { static: true })
+  fourTurfFormulation: ElementRef;
+
+  constructor(
+    private seriesFormulationService: SeriesFormulationService,
+    private renderer: Renderer2
+  ) {}
 
   ngOnInit(): void {
     this.seriesFormulationService.formulationSubject.subscribe(
       ({ seriesName, formula }) => {
         this.formulations = formula;
+
+        if (this.formulations.primary.percentage !== 0) {
+          this.renderer.addClass(
+            this.primaryFormulation.nativeElement,
+            'fadeInPrimary'
+          );
+        }
+        if (this.formulations.secondary.percentage !== 0) {
+          this.renderer.addClass(
+            this.secondaryFormulation.nativeElement,
+            'fadeInSecondary'
+          );
+        }
+        if (this.formulations.fourTurf.percentage !== 0) {
+          this.renderer.addClass(
+            this.fourTurfFormulation.nativeElement,
+            'fadeInFourTurf'
+          );
+        }
+
+        this.renderer.removeClass(
+          this.primaryFormulation.nativeElement,
+          'show'
+        );
+        this.renderer.removeClass(
+          this.secondaryFormulation.nativeElement,
+          'show'
+        );
+        this.renderer.removeClass(
+          this.fourTurfFormulation.nativeElement,
+          'show'
+        );
+
+        this.renderer.addClass(this.primaryFormulation.nativeElement, 'hide');
+        this.renderer.addClass(this.secondaryFormulation.nativeElement, 'hide');
+        this.renderer.addClass(this.fourTurfFormulation.nativeElement, 'hide');
+
+        this.renderer.addClass(
+          this.primaryFormulation.nativeElement,
+          'primaryBackground'
+        );
+
+        this.renderer.addClass(
+          this.fourTurfFormulation.nativeElement,
+          'secondaryBackground'
+        );
+        this.renderer.addClass(
+          this.fourTurfFormulation.nativeElement,
+          'fourTurfBackground'
+        );
+
+        this.renderer.listen(
+          this.primaryFormulation.nativeElement,
+          'animationend',
+          (): void => {
+            this.renderer.removeClass(
+              this.primaryFormulation.nativeElement,
+              'fadeInPrimary'
+            );
+            if (this.formulations.primary.percentage !== 0) {
+              this.renderer.addClass(
+                this.primaryFormulation.nativeElement,
+                'show'
+              );
+              this.renderer.removeClass(
+                this.primaryFormulation.nativeElement,
+                'hide'
+              );
+            }
+          }
+        );
+        this.renderer.listen(
+          this.secondaryFormulation.nativeElement,
+          'animationend',
+          (): void => {
+            this.renderer.removeClass(
+              this.secondaryFormulation.nativeElement,
+              'fadeInSecondary'
+            );
+
+            if (this.formulations.secondary.percentage !== 0) {
+              this.renderer.removeClass(
+                this.secondaryFormulation.nativeElement,
+                'hide'
+              );
+              this.renderer.addClass(
+                this.secondaryFormulation.nativeElement,
+                'show'
+              );
+            }
+          }
+        );
+        this.renderer.listen(
+          this.fourTurfFormulation.nativeElement,
+          'animationend',
+          (): void => {
+            this.renderer.removeClass(
+              this.fourTurfFormulation.nativeElement,
+              'fadeInFourTurf'
+            );
+            if (this.formulations.fourTurf.percentage !== 0) {
+              this.renderer.removeClass(
+                this.fourTurfFormulation.nativeElement,
+                'hide'
+              );
+              this.renderer.addClass(
+                this.fourTurfFormulation.nativeElement,
+                'show'
+              );
+            }
+          }
+        );
       }
     );
   }

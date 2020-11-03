3dc662ab36c9ed34e1d7baf8c89c0d4d40d9bfe0
Jared J. Peters <jjpeters@dlfna.com>
2020-11-03 10:11:25 -0800

    added news service to show and populate news

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {NewsService} from './shared/news.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'competition-grade';
-  newsAvailable = true;
+  newsAvailable: string;
+  constructor(private newsService: NewsService) {
+
+  }
+  ngOnInit(): void {
+   this.newsAvailable = this.newsService.getCurrentNews()
+    
+  }
 }
--- src/app/components/news/news.component.ts
+++ src/app/components/news/news.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NewsService } from 'src/app/shared/news.service';
 
 @Component({
   selector: 'app-news',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./news.component.css']
 })
 export class NewsComponent implements OnInit {
-
-  constructor() { }
+  currentNews: string
+  constructor(private newsService: NewsService) { }
 
   ngOnInit(): void {
+    this.currentNews = this.newsService.getCurrentNews()
   }
 
 }

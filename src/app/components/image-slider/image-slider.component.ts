89fedac0b1b9c7440dd3f768a01510d9336537c8
Jared J. Peters <jjpeters@dlfna.com>
2020-11-04 12:20:42 -0800

    finalizing image slider

--- src/app/components/image-slider/image-slider.component.ts
+++ src/app/components/image-slider/image-slider.component.ts
@@ -1,19 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-image-slider',
   templateUrl: './image-slider.component.html',
   styleUrls: ['./image-slider.component.css'],
 })
-export class ImageSliderComponent implements OnInit {
+export class ImageSliderComponent implements OnInit, OnDestroy {
+  @Input('speed')speed :number;
+  activeImage: string
+  currentIndex: number = 0;
+  images: string[]
+  sliderTimer;
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+  
+    this.images = [
+      '../../../../assets/Competition_Grade_SliderImage_Main.png',
+      '../../../../assets/football_1500x1000.png',
+      '../../../../assets/baseball_1500x1000.png',
+      '../../../../assets/soccer_ball_1500x1000.png',
+    ];
+    this.activeImage = this.images[0];
+    this.startSlider();
+  }
 
   onPreviousImage(): void {
-    alert('Previous Image');
+    if (this.currentIndex > 0) {
+      this.currentIndex--
+    } else {
+      this.currentIndex = this.images.length -1 
+    }
+
+    this.activeImage = this.images[this.currentIndex];
   }
   onNextImage(): void {
-    alert('Next Image');
+    if (this.currentIndex < this.images.length - 1) {
+      this.currentIndex++
+    } else {
+      this.currentIndex = 0
+    }
+    this.activeImage = this.images[this.currentIndex];
+  }
+
+  startSlider(): void {
+    this.sliderTimer = setInterval(() => {
+      this.onNextImage();
+    }, this.speed)
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.sliderTimer);
   }
 }

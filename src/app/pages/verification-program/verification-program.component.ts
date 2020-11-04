c0ee1a2cda817860796437b9e70eca12077b8bea
Jared J. Peters <jjpeters@dlfna.com>
2020-11-04 11:32:03 -0800

    verification program page work

--- src/app/pages/verification-program/verification-program.component.ts
+++ src/app/pages/verification-program/verification-program.component.ts
@@ -6,7 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./verification-program.component.css']
 })
 export class VerificationProgramComponent implements OnInit {
-
+  kbgVarieties = [
+    'Sombrero',
+  'Fielder (SPTR 2959)',
+  'Heidi (A00-2882)',
+  'Jackrabbit',
+  'Granite',
+  'Mercury',
+  'SR 2100',
+  'SR 2150',
+  'SRX 2758'];
+  tfVarieties = [
+    'Crossfire 4',
+  'Fayette',
+  'Standout',
+  'Nightcrawler',
+  'Turfway',
+  'Rhizing Moon',
+  'Firewall',
+  'Houndog 8'];
+  prgVarieties = [
+ 'Karma',
+'Bandalore',
+'Banfield',
+'Aspire',
+'Thrive',
+'Stamina',
+'Wicked',
+'Fiesta 4',
+'Allstar Fore',
+'Fiesta Cinco',
+  ];
   constructor() { }
 
   ngOnInit(): void {

f603ed905f0e092a0f1eefa12e863ab5204d0f5e
Jared Peters <jjpeters@dlfna.com>
2020-11-06 15:49:25 -0800

    styling for navbar

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import { Component, ElementRef, OnChanges, OnInit, Renderer2, ViewChild, ViewEncapsulation } from '@angular/core';
 import { MatMenuTrigger } from '@angular/material/menu';
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { ProductsService } from 'src/app/shared/products.service';
@@ -14,13 +14,29 @@ export class HeaderComponent implements OnInit {
   constructor(
     private productsService: ProductsService,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private renderer: Renderer2
   ) { }
   @ViewChild(MatMenuTrigger) trigger: MatMenuTrigger;
+  @ViewChild('navigation', {static: true}) navigation: ElementRef;
+
+
 
   ngOnInit(): void {
 
+    let nav = this.navigation.nativeElement
+    let offset = nav.offsetTop;
+
+      this.renderer.listen(window, 'scroll', () => {  
+      if(window.pageYOffset <= offset) {
+        this.renderer.removeClass(nav, 'sticky');
+      } else {
+        this.renderer.addClass(nav, 'sticky');
+      }
+    });
   }
+
+
 
  
 }

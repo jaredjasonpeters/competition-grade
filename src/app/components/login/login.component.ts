1e4650e2bbde94638c6861dc47bc45df3bd1b075
Jared Peters <jjpeters@dlfna.com>
2020-11-12 13:48:27 -0800

    login component

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/shared/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+  }
+
+    login(form): void {
+    console.log('LOGGING IN', '..Form', form.value)
+    this.authService.login();
+  }
+
+}

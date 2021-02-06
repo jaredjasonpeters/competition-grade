f9838f10dd802d62efbf3fa7c1d35cc3fb2fa663
jaredjasonpeters <callmerecursive@gmail.com>
2021-02-05 18:02:26 -0800

    fixed some errors with the twitter timeline

--- src/app/components/twitter-feed/twitter-feed.component.ts
+++ src/app/components/twitter-feed/twitter-feed.component.ts
@@ -1,14 +1,12 @@
 import { DOCUMENT } from '@angular/common';
 import {
-  AfterViewChecked,
-  AfterViewInit,
   Component,
+  ElementRef,
   Inject,
   OnDestroy,
+  OnChanges,
   OnInit,
   Renderer2,
-  TemplateRef,
-  ViewContainerRef,
 } from '@angular/core';
 
 @Component({
@@ -16,27 +14,87 @@ import {
   templateUrl: './twitter-feed.component.html',
   styleUrls: ['./twitter-feed.component.css'],
 })
-export class TwitterFeedComponent implements OnInit {
+export class TwitterFeedComponent implements OnInit, OnChanges, OnDestroy {
+  timelineUrl = 'https://twitter.com/CompGradeSeed?ref_src=twsrc%5Etfw';
+  twitterWidgetRef: ElementRef<HTMLScriptElement>;
+  twitterFrameUnlistener;
+
   constructor(
     private renderer: Renderer2,
     @Inject(DOCUMENT) private doc: Document
   ) {}
 
   ngOnInit(): void {
-    function loadTwitterStream(renderer, doc): void {
-      const head = doc.getElementsByTagName('head')[0];
+    this.loadTwitterStream();
+    this.checkIframeLoaded();
+  }
 
-      const twitterScript = renderer.createElement('script');
+  checkIframeLoaded(): void {
+    this.twitterFrameUnlistener = this.renderer.listen(
+      this.doc,
+      'DOMNodeInserted',
+      (ev) => {
+        let regex = new RegExp(/^twitter-widget-\d*/);
+        if (
+          ev.target instanceof HTMLIFrameElement &&
+          regex.test(ev.target.id)
+        ) {
+          let iframe = ev.target;
+          let iframeDoc =
+            iframe.contentDocument || iframe.contentWindow.document;
 
-      renderer.setAttribute(twitterScript, 'async', 'true');
-      renderer.setAttribute(
-        twitterScript,
-        'src',
-        'https://platform.twitter.com/widgets.js'
+          async function checkForIframeLoaded() {
+            let iframeTimeout = setTimeout(() => {
+              if (iframeDoc.readyState === 'complete') {
+                clearTimeout(iframeTimeout);
+                //check if there are children on the body, if not reload the page
+                let bodyTag = iframeDoc.getElementsByTagName('body')[0];
+                if (Array.from(bodyTag.children).length === 0) {
+                  window.location.reload();
+                }
+              } else {
+                checkForIframeLoaded();
+              }
+            }, 1000);
+          }
+          checkForIframeLoaded();
+        }
+      }
+    );
+  }
+
+  loadTwitterStream(): void {
+    console.log('LOADING TWITTER', this.twitterWidgetRef);
+    const head = this.doc.getElementsByTagName('head')[0];
+    const twitterScript = this.renderer.createElement('script');
+    this.renderer.setAttribute(twitterScript, 'async', 'true');
+    this.renderer.setAttribute(
+      twitterScript,
+      'src',
+      'https://platform.twitter.com/widgets.js'
+    );
+    this.renderer.setAttribute(twitterScript, 'charset', 'utf-8');
+    this.twitterWidgetRef = twitterScript;
+    this.renderer.appendChild(head, twitterScript);
+    console.log('NEW SCRIPT', this.twitterWidgetRef);
+  }
+
+  removeTwitterStream() {
+    console.log('SCRIPT TAG', this.twitterWidgetRef);
+    if (this.twitterWidgetRef) {
+      console.log('REMOVING SCRIPT');
+      this.renderer.removeChild(
+        this.doc.getElementsByTagName('head')[0],
+        this.twitterWidgetRef
       );
-      renderer.setAttribute(twitterScript, 'charset', 'utf-8');
-      renderer.appendChild(head, twitterScript);
+      this.twitterWidgetRef = null;
     }
-    loadTwitterStream(this.renderer, this.doc);
+  }
+
+  ngOnChanges(): void {}
+
+  ngOnDestroy(): void {
+    this.removeTwitterStream();
+    this.twitterFrameUnlistener();
   }
 }

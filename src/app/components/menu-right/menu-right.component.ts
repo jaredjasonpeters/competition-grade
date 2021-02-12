7272d0eff3f30fbebe1e72b69b64240515410eea
jaredjasonpeters <callmerecursive@gmail.com>
2021-02-11 22:11:25 -0800

    fixed features selection menu right

--- src/app/components/menu-right/menu-right.component.ts
+++ src/app/components/menu-right/menu-right.component.ts
@@ -23,38 +23,37 @@ import { LayoutService } from 'src/app/shared/layout.service';
   providers: [LayoutService],
 })
 export class MenuRightComponent implements OnInit, OnChanges {
-  largeBreakpoints = {
-    colSize: 6,
-    rowSpan_V: 7,
-    rowSpan_F: 2,
-    rowSpan_S: 5,
-    rowHeight: 725 / 14 + 'px',
-  };
+  // largeBreakpoints = {
+  //   colSize: 6,
+  //   rowSpan_V: 7,
+  //   rowSpan_F: 2,
+  //   rowSpan_S: 5,
+  //   rowHeight: 725 / 14 + 'px',
+  // };
 
-  smallBreakpoints = {
-    colSize: 1,
-    rowSpan_V: 1,
-    rowSpan_F: 1,
-    rowSpan_S: 1,
-    rowHeight: '33.3%',
-  };
+  // smallBreakpoints = {
+  //   colSize: 1,
+  //   rowSpan_V: 1,
+  //   rowSpan_F: 1,
+  //   rowSpan_S: 1,
+  //   rowHeight: '33.3%',
+  // };
 
-  breakpoint;
+  // breakpoint;
 
   constructor(
     private renderer: Renderer2,
     private route: ActivatedRoute,
-    private featuredVideoService: FeaturedVideoService,
-    public layoutService: LayoutService
+    private featuredVideoService: FeaturedVideoService // public layoutService: LayoutService
   ) {}
   @Input() page;
-  @Input() features: string[] = ['all'];
+  @Input() features: string[];
   @ViewChild('menu', { static: true }) menu: ElementRef;
   @ViewChild('videoTile', { static: true }) videoTile: ElementRef;
 
   ngOnInit(): void {
     this.featuredVideoService.setVideoByPage(this.page);
-    this.route.parent.url.subscribe((url) => console.log('URL', url));
+    // this.route.parent.url.subscribe((url) => console.log('URL', url));
   }
 
   ngOnChanges(): void {

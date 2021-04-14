70d3db0a6593193f963c37daaf8aa5460bb3b851
jaredjasonpeters <callmerecursive@gmail.com>
2021-04-14 13:46:50 -0700

    fixed projects for mobile nav based on config

--- src/app/components/mobile/mobile-nav/mobile-nav.component.ts
+++ src/app/components/mobile/mobile-nav/mobile-nav.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { MatMenuTrigger } from '@angular/material/menu';
+import { ProjectsService } from 'src/app/shared/projects.service';
 
 @Component({
   selector: 'app-mobile-nav',
   templateUrl: './mobile-nav.component.html',
   styleUrls: ['./mobile-nav.component.css'],
-  encapsulation: ViewEncapsulation.None,
+  encapsulation: ViewEncapsulation.None
 })
 export class MobileNavComponent implements OnInit {
   @ViewChild(MatMenuTrigger) trigger: MatMenuTrigger;
-  constructor() {}
+  constructor(public projectsService: ProjectsService) {}
 
   ngOnInit(): void {}
 
